a837113e01842234cbd9b71bcaf1ded564ffe56d
anshultyag <you@example.com>
2022-01-04 02:23:30 +0530

    Added: Assume 20 working days per month

--- src/com/company/EmpWage.java
+++ src/com/company/EmpWage.java
@@ -3,21 +3,31 @@ package com.company;
 public class EmpWage {
     static final int PART_TIME = 0;
     static final int FULL_TIME = 1;
-    public static void computeEmpWage(int wagePerHour) {
+    public static void computeEmpWage(int wagePerHour,int numOfWorkingDays) {
         int empHrs=0;
-        int empCheck = (int) Math.floor(Math.random() * 10) % 2;
-        switch (empCheck) {
-            case(PART_TIME):
-                System.out.println("Part Time :");
-                empHrs =8;
-            case(FULL_TIME):
-                System.out.println("Full time  :");
-                empHrs =4;
+        int totalEmpHrs = 0;
+        int totalWage = 0;
+        int totalWorkingDays=0;
+        while(totalWorkingDays <= numOfWorkingDays) {
+             int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+             switch (empCheck) {
+                case (PART_TIME):
+                     empHrs = 8;
+                     break;
+                case (FULL_TIME):
+                     empHrs = 4;
+                     break;
+                default:
+                    empHrs = 0;
+                    break;
+             }
+            totalEmpHrs= empHrs + 0;
+            totalWorkingDays++;
         }
-        int DailyWage=wagePerHour * empHrs;
-        System.out.println("Total Emp wage : " + DailyWage );
+        totalWage=  totalEmpHrs * wagePerHour;
+        System.out.println("Total Emp wage : " + totalWage );
     }
     public static void main(String[] args) {
-        computeEmpWage(20);
+        computeEmpWage(20,21);
     }
 }
